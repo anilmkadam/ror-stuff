b6785afdb2eeab5221c8dd5c226e6d781c5fac65
Anil Kadam <anilkadam210@gmail.com>
2019-01-09 11:05:18 +0530

    Some modification

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
 end
